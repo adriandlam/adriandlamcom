eb1a080a85341b1fc0f6ee13a04c81b323553449
Adrian Lam <adrianlamdev@gmail.com>
2025-03-08 23:42:30 -0800

    feat(projects): add redis cpp blog

--- data/resume.ts
+++ data/resume.ts
@@ -9,27 +9,6 @@ const RESUME = {
 	},
 	projects: [
 		{
-			slug: "adrians-research-engine",
-			name: "Adrian's Research Engine",
-			description:
-				"An AI-powered research engine that helps you find relevant papers and insights for your research from natural language.",
-			longDescription:
-				"Adrian's Research Engine is a research assistant that helps you find relevant papers and insights for your research from natural language. It uses a hybrid retrieval system that combines light weight semantic search with BM25 to improve query results, with reranking in order to determine relevance. I'm currently working on it!",
-			stack: [
-				"Next.js",
-				"Express",
-				"Redis",
-				"Vector Embeddings",
-				"TypeScript",
-				"TailwindCSS",
-			],
-			keyFeatures: [
-				"Hybrid retrieval combining light weight semantic search with BM25",
-				"Reranking in order to determine relevance",
-			],
-			year: 2025,
-		},
-		{
 			slug: "heida",
 			name: "Heida",
 			description:
@@ -141,6 +120,38 @@ const RESUME = {
 				"Interactive visualization dashboard",
 			],
 			year: 2024,
+		},
+		{
+			slug: "redis-cpp",
+			name: "Redis C++ (WIP)",
+			description:
+				"A C++ library for Redis that provides a high-performance, thread-safe, and easy-to-use interface for interacting with Redis databases.",
+			longDescription:
+				"Redis C++ is a C++ library for Redis that provides a high-performance, thread-safe, and easy-to-use interface for interacting with Redis databases.",
+			githubUrl: "https://github.com/adriandlam/redis-cpp",
+			stack: ["C++", "Redis", "CMake", "GitHub Actions"],
+			year: 2025,
+		},
+		{
+			slug: "adrians-research-engine",
+			name: "Adrian's Research Engine (WIP)",
+			description:
+				"An AI-powered research engine that helps you find relevant papers and insights for your research from natural language.",
+			longDescription:
+				"Adrian's Research Engine is a research assistant that helps you find relevant papers and insights for your research from natural language. It uses a hybrid retrieval system that combines light weight semantic search with BM25 to improve query results, with reranking in order to determine relevance.",
+			stack: [
+				"Next.js",
+				"Express",
+				"Redis",
+				"Vector Embeddings",
+				"TypeScript",
+				"TailwindCSS",
+			],
+			keyFeatures: [
+				"Hybrid retrieval combining light weight semantic search with BM25",
+				"Reranking in order to determine relevance",
+			],
+			year: 2025,
 		},
 		{
 			slug: "wellbeing-analyzer",
