af4a107d238d5f38cee19741725a32b4bae7d588
Adrian Lam <me@adriandlam.com>
2025-11-10 21:44:39 -0800

    fix: wrong motion import

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 
 function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
 	return (
