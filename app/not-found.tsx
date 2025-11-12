6669c5d2f1f0f4968e39529ac128e3e3dddac25a
Adrian Lam <me@adriandlam.com>
2025-11-11 22:02:14 -0800

    fix not found page styling

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,15 +2,33 @@ import Link from "next/link";
 
 export default function NotFound() {
 	return (
-		<div className="bg-background fixed top-0 left-0 w-screen h-screen flex flex-col justify-center items-center text-center px-4">
-			<span className="text-3xl">404</span>
-			<h1 className="text-5xl tracking-tight mb-2">You found a dead link</h1>
-			<p className="text-muted-foreground">
-				I haven&apos;t built this page yet, but you could always go back{" "}
-				<Link href="/" className="underline underline-offset-4">
-					home
-				</Link>
-			</p>
-		</div>
+		<main>
+			<div>
+				<p className="text-accent-foreground tracking-widest font-mono text-sm">
+					404
+				</p>
+				<h1 className="mt-1.5 text-4xl">Page not found</h1>
+				<p className="mt-2 text-muted-foreground">
+					This page doesn&apos;t exist or has been moved.
+				</p>
+			</div>
+			<div className="mt-4">
+				<p>
+					You can head back{" "}
+					<Link href="/" className="link">
+						home
+					</Link>{" "}
+					or check out some of my{" "}
+					<Link href="/projects" className="link">
+						projects
+					</Link>{" "}
+					and{" "}
+					<Link href="/blog" className="link">
+						blog posts
+					</Link>
+					.
+				</p>
+			</div>
+		</main>
 	);
 }
